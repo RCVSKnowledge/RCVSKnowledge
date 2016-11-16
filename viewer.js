1c02da2ec381ecbb93cef1785839e6c8b478b9a2
John <john-root@users.noreply.github.com>
2016-11-16 09:14:46 +0000

    added uv and updated configuration

---
+++ viewer.js
@@ -0,0 +1,63 @@
+$(function() {
+	
+
+    var bootstrapper;
+    var isFullSreen = false;
+
+    var $header = $('header');
+    var $manifest = $('#manifest');
+    var $main = $('main');
+    var $uv = $('#uv');
+    var $footer = $('footer');
+
+    window.onresize = function() {
+    	resize();
+    };
+	   
+    function resize() {
+    	var windowWidth = window.innerWidth;
+        var windowHeight = window.innerHeight;
+        var height = (isFullSreen) ? windowHeight : windowHeight - $header.outerHeight();
+        $main.height(height);
+        $main.width(windowWidth);
+        $uv.width(windowWidth);
+        $uv.height(height);
+    }
+	
+    function loadViewer() {
+		
+           // todo: update embed.js to work with script loaders.
+           if (window.initPlayers && window.easyXDM) {
+               initPlayers($('.uv'));
+           } else {
+               setTimeout(loadViewer, 100);
+           }
+    }
+	
+    function setSelectedManifest(manifestUri){
+
+    	if (!manifestUri){
+        	manifestUri = Utils.Urls.GetQuerystringParameter('manifest');
+        }
+
+        if (manifestUri && manifestUri.startsWith("http://waylon.rcvsvethistory.org/")) {
+        
+            $('.uv').attr('data-uri', manifestUri);
+        }
+    }	
+	
+	function init() {
+			
+		resize();
+
+        // append uv script
+        $('body').append('<script type="text/javascript" id="embedUV" src="/uv/lib/embed.js"><\/script>');
+
+        setSelectedManifest();
+
+        loadViewer();
+    }
+
+    init();
+	
+});
